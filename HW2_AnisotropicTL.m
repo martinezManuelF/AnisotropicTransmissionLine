25ecd9d9e7605a156c3260e4c0f5b33fa138cd98
Manuel Martinez <manuel.f.m.soto@gmail.com>
2018-01-31 10:02:33 -0800

    Made Changes to HW2_AnisotropicTL.m

--- HW2_AnisotropicTL.m
+++ HW2_AnisotropicTL.m
@@ -27,7 +27,7 @@ seconds     = 1;
 hertz       = 1/seconds;
 kilohertz   = 1e3 * hertz;
 megahertz   = 1e6 * hertz;
-gigahertz   = 1e9 * hertz
+gigahertz   = 1e9 * hertz;
 
 % CONSTANTS
 e0 = 8.85418782e-12;
@@ -46,3 +46,39 @@ figure('Color','w');
 NC = 1; % Number of conductors
 ersup = 1.0; % Permittivity of Superstrate
 ersub = 9.0; % Permittivity of Substrate
+h = 2; % Height of substrate
+w = 4; % Width of TL
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% DEFINE GRID
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% GRID PARAMETERS
+Sx = w;
+Sy = h;
+Nx = 256;
+Ny = 256;
+
+% INITIAL GUESS AT GRID RESOLUTION
+dx = Sx/Nx;
+dy = Sy/Ny;
+
+% COMPUTE 2x GRID
+Nx2 = 2*Nx;
+dx2 = dx/2;
+Ny2 = 2*Ny;
+dy2 = dy/2;
+
+% SNAP GRID TO CRITICAL DIMENSIONS
+nx = ceil(w/dx);
+dx = w/nx;
+ny = ceil(h/dy);
+dy = h/ny;
+
+% GRID AXES
+xa = [0:Nx-1]*dx; xa = xa - mean(xa);
+ya = [0:Ny-1]*dy; ya = ya - mean(ya);
+
+% 2x GRID AXES
+xa2 = [0:Nx2-1]*dx2; xa2 = xa2 - mean(xa2);
+ya2 = [0:Ny2-1]*dy2; ya2 = ya2 - mean(ya2);
